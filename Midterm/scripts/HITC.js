681f44631b56b339607ea6da98337f6084f01567
Easton Kang <euisungkang@gmail.com>
2020-10-26 14:18:32 -0400

    Midterm

---
+++ Midterm/scripts/HITC.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+
+    $(window).resize(function(){
+        $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+    });
+});
+  
